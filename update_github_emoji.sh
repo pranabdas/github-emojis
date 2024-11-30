be2abf817d1c67f77fb015bc598be6e7f94b8979
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2024-11-30 07:09:17 +0000

    update emojis + add emojis by unicode

--- update_github_emoji.sh
+++ update_github_emoji.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 if [ -d "assets" ]; then
   rm -rf assets
 fi
-mkdir assets
+mkdir -p assets/png
+mkdir assets/unicode
 
 wget https://api.github.com/emojis -O data.json
 
@@ -14,15 +16,17 @@ fid.close()
 
 fid = open('wget_urls', 'w')
 for key, value in data.items():
-    fid.write("wget {0} -O assets/{1}.png\n".format(value, key))
+    fid.write("wget {0} -O assets/png/{1}.png\n".format(value, key))
+    unicode = value.split("/")[-1].split("?")[0]
+    fid.write("cp assets/png/{0}.png assets/unicode/{1}\n".format(key, unicode))
 fid.close()
 
 fid = open('README.md', 'w')
 fid.write("# GitHub emoji assets\n\nThis repository contains the GitHub emoji assets in PNG format. Here are all the {0} emojis:\n\nName | Emoji\n---- | -----\n".format(len(data)))
 for key in data.keys():
-    fid.write('{0} | <img src="assets/{0}.png" alt="{0}" width=20/>\n'.format(key))
+    fid.write('{0} | <img src="assets/png/{0}.png" alt="{0}" width=20/>\n'.format(key))
 fid.close()
 EOF
 
-sh wget_urls
+bash wget_urls
 rm data.json wget_urls
